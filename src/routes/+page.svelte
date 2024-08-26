03a0c13ad2bc2d376b19cd9b5d4dbe18b90cbb6a
Gudster <g.fristedt@gmail.com>
2024-08-26 13:23:25 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="font-mono container flex">
+	<h1 class="text-6xl font-black tracking-tight">Food Playlists</h1>
+	<h2 class="font-thin tracking-tight">
+		Get a playlist for your dinner based on the music you love.
+	</h2>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.200);
+	}
+</style>
